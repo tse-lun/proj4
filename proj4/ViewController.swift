ca480b6a0f7f4fb270767f1a9adfd0332d43199b
tse-lun <tse_lun@hotmail.com>
2025-03-28 23:02:36 +0800

    Initial Commit

---
+++ proj4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proj4
+//
+//  Created by Stephen Chiu on 2025-03-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
